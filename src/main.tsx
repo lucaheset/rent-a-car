384aa6706a7190d7d26610f9ff967d2cc298e464
010754L3 <luca.monte@sefaz.ce.gov.br>
2025-02-18 14:01:06 -0300

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import HomePage from './pages/HomePage/HomePage.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+    <HomePage />
+  </StrictMode>,
+)
